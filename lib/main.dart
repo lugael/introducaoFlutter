756d9424f545272b39fe496009ea1d58c304d44f
Maria Clara <mcfranca0107@gmail.com>
2022-05-03 17:18:28 -0300

    Aprendendo a manipular as pages

--- lib/main.dart
+++ lib/main.dart
@@ -83,29 +83,21 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
+              style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Icon(Icons.warning, size: 120.0, color: Colors.red),
+            Image.network('https://d3ugyf2ht6aenh.cloudfront.net/stores/001/205/883/products/quadro-tio-patinhas-culpado-por-fazer-o-dinheiro-trabalhar-sem-descanso-c4d7999e794fd408a316161153145639-640-0.jpg')
           ],
         ),
       ),
